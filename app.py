eb2a2a47448e814c256de8d2aadc1c23e816e8cd
Benjamin Patch <ben@benjaminpatch.com>
2025-04-25 12:39:45 -0700

    Declare the Reader model

--- app.py
+++ app.py
@@ -16,9 +16,20 @@ class Book(db.Model):
   month = db.Column(db.String(20), index = True, unique = False) # the month of book suggestion
   year = db.Column(db.Integer, index = True, unique = False) # the year of book suggestion
   
-  # get a nice printout for Book objects
+  # printout for Book objects
   def __repr__(self):
     return "{} in: {},{}".format(self.title, self.month,self.year)
+
+# declare the Reader model
+class Reader(db.Model):
+  id = db.Column(db.Integer, primary_key = True)
+  name = db.Column(db.String(50), index = True, unique = False)
+  surname = db.Column(db.String(80), index = True, unique = False)
+  email = db.Column(db.String(120), index = True, unique = True)
+
+  # printout for Reader objects
+  def __repr__(self):
+    return "Reader: {}".format(self.email)
 
 # index route
 @app.route('/')
